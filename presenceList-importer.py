767c960053f6422ac61f3743911b21cdb32955fe
Uwe van Heesch <uwe@vanheesch.net>
2017-09-19 14:01:32 +0200

    Added check for existing tuples so the script can be called multiple times per day.

--- presenceList-importer.py
+++ presenceList-importer.py
@@ -1,16 +1,42 @@
+#!/usr/bin/python3
+
+# Retrieves presenceList files from iSAS, parses them and adds data to the Ebig InfluxDB
+# Fileformat:
+# 0: AanwezigheidsregistratieID;
+# 1: Datum v registreren;
+# 2:  Lokaal;
+# 3:  Rooster-timeslot van;
+# 4: Rooster-timeslot tot;
+# 5: startmoment aanwezigheidsregistratrie;
+# 6: aantal aanwezig;
+# 7: aantal te laat
+
 from influxdb import InfluxDBClient
 import datetime
 import arrow
 import pytz
 import sys
 
+from pathlib import Path
+
 if sys.version_info[0] >= 3:
     from urllib.request import urlretrieve
 else:
-    # Not Python 3 - today, it is most likely to be Python 2
-    # But note that this might need an update when Python 4
-    # might be around one day
     from urllib import urlretrieve
+
+existingTuples = set()
+
+def readExistingPresenceItems(registrationDate):
+    existingPresenceFile = Path(registrationDate)
+    if existingPresenceFile.is_file():
+        with open(registrationDate, "r") as inFile:
+            for line in inFile:
+                print("Existing line: %s" %line)
+                tuples = line.split(";")
+                if(tuples.__len__() <= 7):
+                    continue
+                else:
+                    existingTuples.add(tuples[0])
 
 def influx_handler(room, registrationTime, numPeople):
     series = []
@@ -36,33 +62,25 @@ room_dict = {'D104': 'R26-D-1.04',
 currentDate = arrow.now().format('YYYY-MM-DD')
 local_tz = pytz.timezone('Europe/Amsterdam')
 
-
 print("Importing presence list for %s" % currentDate)
 url = "http://sascalendar.han.nl/getrss.aspx?id=audfp&type=UWE_AWR_HTML:" + currentDate
+readExistingPresenceItems(currentDate)
 urlretrieve (url, currentDate)
-
-
-# 0: AanwezigheidsregistratieID;
-# 1: Datum v registreren;
-# 2:  Lokaal;
-# 3:  Rooster-timeslot van;
-# 4: Rooster-timeslot tot;
-# 5: startmoment aanwezigheidsregistratrie;
-# 6: aantal aanwezig;
-# 7: aantal te laat
-
 
 with open(currentDate, "r") as inFile:
     for line in inFile:
         if(line.split(";").__len__() <= 7):
             continue
-        room = line.split(";")[2]
-        registrationTime = line.split(";")[5]
-        numPresent = int(line.split(";")[6])
-        numTooLate = int(line.split(";")[7])
+        presenceTuple = line.split(";")
+        if(existingTuples.__contains__(presenceTuple[0])):
+            print("Skipping existing tuple.")
+            continue
+        room = presenceTuple[2]
+        registrationTime = presenceTuple[5]
+        numPresent = int(presenceTuple[6])
+        numTooLate = int(presenceTuple[7])
         dateFormatted = currentDate + "T" + registrationTime + "Z"
 
         datetime_local = local_tz.localize(datetime.datetime.strptime(dateFormatted, '%Y-%m-%dT%H:%M:%SZ'),is_dst=None)
         datetime_utc = datetime_local.astimezone(pytz.utc)
-        print(datetime_utc)
         influx_handler(room_dict[room], datetime_utc, numPresent + numTooLate)
